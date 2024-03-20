7449d5e1b1cf080516d92c82195e35133dec175c
Zaheer Ahmad <zaheerahmaduom@gmail.com>
2024-03-20 23:46:06 +0500

    base url added

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -1,34 +1,34 @@
     <!-- jQuery -->
-    <script src="<?= base_url() ?>assets/adminlte/plugins/jquery/jquery.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/jquery/jquery.min.js"></script>
     <!-- jQuery UI 1.11.4 -->
-    <script src="<?= base_url() ?>assets/adminlte/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/jquery-ui/jquery-ui.min.js"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
       $.widget.bridge('uibutton', $.ui.button)
     </script>
     <!-- Bootstrap 4 -->
-    <script src="<?= base_url() ?>assets/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- ChartJS -->
-    <script src="<?= base_url() ?>assets/adminlte/plugins/chart.js/Chart.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/chart.js/Chart.min.js"></script>
     <!-- Sparkline -->
-    <script src="<?= base_url() ?>assets/adminlte/plugins/sparklines/sparkline.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/sparklines/sparkline.js"></script>
     <!-- JQVMap -->
-    <script src="<?= base_url() ?>assets/adminlte/plugins/jqvmap/jquery.vmap.min.js"></script>
-    <script src="<?= base_url() ?>assets/adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/jqvmap/jquery.vmap.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
     <!-- jQuery Knob Chart -->
-    <script src="<?= base_url() ?>assets/adminlte/plugins/jquery-knob/jquery.knob.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/jquery-knob/jquery.knob.min.js"></script>
     <!-- daterangepicker -->
-    <script src="<?= base_url() ?>assets/adminlte/plugins/moment/moment.min.js"></script>
-    <script src="<?= base_url() ?>assets/adminlte/plugins/daterangepicker/daterangepicker.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/moment/moment.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/daterangepicker/daterangepicker.js"></script>
     <!-- Tempusdominus Bootstrap 4 -->
-    <script src="<?= base_url() ?>assets/adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
     <!-- Summernote -->
-    <script src="<?= base_url() ?>assets/adminlte/plugins/summernote/summernote-bs4.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/summernote/summernote-bs4.min.js"></script>
     <!-- overlayScrollbars -->
-    <script src="<?= base_url() ?>assets/adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
     <!-- AdminLTE App -->
-    <script src="<?= base_url() ?>assets/adminlte/dist/js/adminlte.js"></script>
-    <script src="<?= base_url() ?>assets/adminlte/dist/js/pages/dashboard.js"></script>
+    <script src="<?= ASSETS ?>adminlte/dist/js/adminlte.js"></script>
+    <script src="<?= ASSETS ?>adminlte/dist/js/pages/dashboard.js"></script>
 </body>
 
 </html>
--- application/views/inc/sidebar.php
+++ application/views/inc/sidebar.php
@@ -2,7 +2,7 @@
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
      <!-- Brand Logo -->
      <a href="index3.html" class="brand-link">
-         <img src="<?= base_url() ?>assets/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+         <img src="<?= ASSETS ?>adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
          <span class="brand-text font-weight-light">Roshan Tea</span>
      </a>
 
@@ -11,7 +11,7 @@
          <!-- Sidebar user panel (optional) -->
          <div class="user-panel mt-3 pb-3 mb-3 d-flex">
              <div class="image">
-                 <img src="<?= base_url() ?>assets/adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                 <img src="<?= ASSETS ?>adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
              </div>
              <div class="info">
                  <a href="#" class="d-block">Alexander Pierce</a>
@@ -60,6 +60,14 @@
                          <i class="nav-icon fas fa-edit"></i>
                          <p>
                              Records
+                         </p>
+                     </a>
+                 </li>
+                 <li class="nav-item">
+                     <a href="<?= BASE_URL?>/auth/logout" class="nav-link">
+                         <i class="nav-icon fas fa-edit"></i>
+                         <p>
+                             Logout
                          </p>
                      </a>
                  </li>
