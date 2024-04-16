a2476657fd53356abe7fff851ea687bfdc198bdf
AliFarukk <alifaruq786@gmail.com>
2024-04-16 20:02:32 +0500

    Records page and controller added with datatables

--- application/views/admin_dashboard/inc/footer.php
+++ application/views/admin_dashboard/inc/footer.php
@@ -4,7 +4,7 @@
     <script src="<?= ASSETS ?>adminlte/plugins/jquery-ui/jquery-ui.min.js"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
-      $.widget.bridge('uibutton', $.ui.button)
+    	$.widget.bridge('uibutton', $.ui.button)
     </script>
     <!-- Bootstrap 4 -->
     <script src="<?= ASSETS ?>adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -27,6 +27,38 @@
     <script src="<?= ASSETS ?>adminlte/plugins/toastr/toastr.min.js"></script>
     <!-- AdminLTE App -->
     <script src="<?= ASSETS ?>adminlte/dist/js/adminlte.js"></script>
-</body>
+    <!-- Datatables -->
+    <script src="<?= ASSETS ?>adminlte/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/jszip/jszip.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/pdfmake/pdfmake.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/pdfmake/vfs_fonts.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+    <script src="<?= ASSETS ?>adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+    <script>
+    	$(function() {
+    		$("#example1").DataTable({
+    			"responsive": true,
+    			"lengthChange": false,
+    			"autoWidth": false,
+    			"buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    		}).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    		$('#example2').DataTable({
+    			"paging": true,
+    			"lengthChange": false,
+    			"searching": false,
+    			"ordering": true,
+    			"info": true,
+    			"autoWidth": false,
+    			"responsive": true,
+    		});
+    	});
+    </script>
+    </body>
 
-</html>
+    </html>
--- application/views/admin_dashboard/inc/sidebar.php
+++ application/views/admin_dashboard/inc/sidebar.php
@@ -64,7 +64,7 @@
                      </a>
                  </li>
                  <li class="nav-item">
-                     <a href="#" class="nav-link">
+                     <a href="<?= BASE_URL ?>records" class="nav-link">
                          <i class="nav-icon fas fa-edit"></i>
                          <p>
                              Records
