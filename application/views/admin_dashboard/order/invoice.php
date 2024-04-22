2c7690917be7530ae7d9a18d4291ef15b9068615
Zaheer Ahmad <zaheerahmaduom@gmail.com>
2024-04-22 22:05:22 +0500

    invoice design updated.

--- application/views/admin_dashboard/order/invoice.php
+++ application/views/admin_dashboard/order/invoice.php
@@ -1,75 +1,167 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Order Receipt</title>
+    <meta charset="utf-8" />
+    <title>Invoice</title>
 
     <style>
-        .header {
-            display: flex;
-            display: -webkit-box;
-            justify-content: space-between;
-            align-items: center;
-            margin-bottom: 20px;
+        .invoice-box {
+            max-width: 800px;
+            margin: auto;
+            padding: 30px;
+            font-size: 16px;
+            line-height: 24px;
+            font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+            color: #555;
         }
 
-        .row {
-            display: flex;
-            display: -webkit-box;
+        .invoice-box table {
+            width: 100%;
+            line-height: inherit;
+            text-align: left;
         }
 
-        .logo {
-            width: 100px;
-            height: auto;
+        .invoice-box table td {
+            padding: 5px;
+            vertical-align: top;
         }
 
-        .contact-info {
+        .invoice-box table tr td:nth-child(2) {
             text-align: right;
+        }
+
+        .invoice-box table tr.top table td {
+            padding-bottom: 20px;
+        }
+
+        .invoice-box table tr.top table td.title {
+            font-size: 45px;
+            line-height: 45px;
+            color: #333;
+        }
+
+        .invoice-box table tr.information table td {
+            padding-bottom: 40px;
+        }
+
+        .invoice-box table tr.heading td {
+            background: #eee;
+            border-bottom: 1px solid #ddd;
+            font-weight: bold;
+        }
+
+        .invoice-box table tr.details td {
+            padding-bottom: 20px;
+        }
+
+        .invoice-box table tr.item td {
+            border-bottom: 1px solid #eee;
+        }
+
+        .invoice-box table tr.item.last td {
+            border-bottom: none;
+        }
+
+        .invoice-box table tr.total td:nth-child(2) {
+            border-top: 2px solid #eee;
+            font-weight: bold;
+        }
+
+        @media only screen and (max-width: 600px) {
+            .invoice-box table tr.top table td {
+                width: 100%;
+                display: block;
+                text-align: center;
+            }
+
+            .invoice-box table tr.information table td {
+                width: 100%;
+                display: block;
+                text-align: center;
+            }
+        }
+
+        /** RTL **/
+        .invoice-box.rtl {
+            direction: rtl;
+            font-family: Tahoma, 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
+        }
+
+        .invoice-box.rtl table {
+            text-align: right;
+        }
+
+        .invoice-box.rtl table tr td:nth-child(2) {
+            text-align: left;
         }
     </style>
 </head>
 
 <body>
-    <div class="wrapper">
-        <div class="header">
-            <img src="<?php echo BASE_URL.'assets/images/logo.jpg'; ?>" alt="Logo" class="logo" style="float: left;">
-            <div class="contact-info">
-                <p>Roshan tea company plot no# 5, old sabzi mandi<br> bhalwal, district sargodha</p>
-                <p>+923077713000</p>
-            </div>
-        </div>
-        <div class="content">
-            <h2>Order Information</h2>
-            <?php if ($order_details) : ?>
-                <p>Order ID: <?= $order_details[0]->order_id; ?></p>
-                <div class="row">
-                    <div style="width: 50%;">
-                        <h4>Product Details</h4>
-                        <?php
-                        $totalAmount = 0;
-                        foreach ($order_details as $order) :
-                            $product = get_order_product_details($order->product_id);
-                            $totalAmount += $order->total;
-                        ?>
-                            <p>Product Name: <?= $product->product_name ?></p>
-                            <p>Total Price: <?= $order->total ?></p>
-                        <?php endforeach; ?>
-                    </div>
-                    <div style="width: 50%;">
-                        <h4>Vendor Information</h4>
-                        <p><?= $order_details[0]->business_name ?></p>
-                        <p><?= $order_details[0]->phone_no ?></p>
-                        <p><?= $order_details[0]->address ?></p><br>
-                    </div>
-                </div>
+    <div class="invoice-box">
+        <table cellpadding="0" cellspacing="0">
+            <tr class="top">
+                <td colspan="2">
+                    <table>
+                        <tr>
+                            <td class="title">
+                                <img src="<?php echo BASE_URL . 'assets/images/logo.jpg'; ?>" style="width: 100%; max-width: 110px" />
+                            </td>
+                            <td>
+                                Order ID#: <?= $order_details[0]->order_id; ?><br />
+                                Created: <?php
+                                            $currentDate = date("Y-m-d");
+                                            $formattedDate = date("F j, Y", strtotime($currentDate));
+                                            echo $formattedDate;
+                                            ?>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+
+            <tr class="information">
+                <td colspan="2">
+                    <table>
+                        <tr>
+                            <td>
+                                Roshan Tea Company <br />
+                                Plot no# 5, old sabzi mandi<br />
+                                Bhalwal, district sargodha
+                            </td>
+                            <td>
+                                <?= $order_details[0]->business_name ?><br />
+                                <?= $order_details[0]->phone_no ?><br />
+                                <?= $order_details[0]->address ?>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+
+            <tr class="heading">
+                <td>Product Item</td>
+                <td>Price</td>
+            </tr>
+
             <?php
-            endif; ?>
-            <div>
-                <p>Sub Total: <?= $totalAmount ?></p>
-            </div>
-        </div>
+            $totalAmount = 0;
+            foreach ($order_details as $order) :
+                $product = get_order_product_details($order->product_id);
+                $totalAmount += $order->total;
+            ?>
+                <tr class="item">
+                    <td><?= $product->product_name ?></td>
+                    <td><?= $order->total ?></td>
+                </tr>
+            <?php endforeach; ?>
+
+            <tr class="total">
+                <td></td>
+                <td>Total: <?= $totalAmount ?></td>
+            </tr>
+        </table>
     </div>
 </body>
 
